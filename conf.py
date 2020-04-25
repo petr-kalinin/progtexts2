2fd18be6df44070a2020de55a8ec58a608771ebb
Petr Kalinin <petr@kalinin.nnov.ru>
2020-04-25 21:05:19 +0300

    Basic settings

--- conf.py
+++ conf.py
@@ -18,9 +18,9 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'ProgTexts'
-copyright = '2020, Petr Kalinin'
-author = 'Petr Kalinin'
+project = 'Заметки'
+copyright = '2020, Петр Калинин'
+author = 'Петр Калинин'
 
 
 # -- General configuration ---------------------------------------------------
@@ -55,11 +55,16 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    "collapse_navigation": False
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+highlight_language = "pascal"
 
 #---
 master_doc = 'index'
